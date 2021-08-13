c9e40f63f91727e21a1abb59d7dbcd2248a1b25f
qasabolat <72704352+qasabolat@users.noreply.github.com>
2021-08-13 21:29:56 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// alert("JS IS READY")
+
+
+// JQUERY START
+$(document).ready(function() {
+    $("button").click(function() {
+        alert("sdf")
+        $("p").hide();
+    });
+});
